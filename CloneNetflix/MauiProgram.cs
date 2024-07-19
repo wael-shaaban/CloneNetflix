1a10375c8f905fad7ed0941e76a0883a73f7c401
HackerWael <HackerWael@DESKTOP-UBD6TT5>
2024-07-19 09:46:19 +0300

    Add project files.

---
+++ CloneNetflix/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CloneNetflix
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("Poppins-Regular.ttf", "PoppinsRegular");
+                    fonts.AddFont("Poppins-SemiBold.ttf", "PoppinsSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
